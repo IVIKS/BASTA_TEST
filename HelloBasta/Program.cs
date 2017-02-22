3eb7b4274ceede48c72f53decd9966abb6a8bdab
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-02-22 15:01:31 +0100

    Changed Program

--- HelloBasta/Program.cs
+++ HelloBasta/Program.cs
@@ -7,6 +7,7 @@ namespace HelloBasta
     static void Main(string[] args)
     {
       Console.WriteLine("Hello Basta!");
+      Console.WriteLine("How are you?");
       Console.ReadLine();
     }
   }
