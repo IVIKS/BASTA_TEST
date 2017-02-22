bb6cf8a3abd9b6925e9a816efa1d5acf0d8213d4
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-02-22 14:59:57 +0100

    Console app angelegt

---
+++ HelloBasta/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloBasta
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Basta!");
+      Console.ReadLine();
+    }
+  }
+}
